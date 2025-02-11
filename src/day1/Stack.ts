68e82da8e3e5416fe5471c072f60b14273297a95
Jeann Porfírio <jeannporfirio@gmail.com>
2025-02-11 14:28:07 -0300

    stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,29 @@
+type Node<T> = {
+    val: T;
+    next?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    public head?: Node<T>;
 
     constructor() {
+        this.length = 0;
     }
 
     push(item: T): void {
-
-}
+        this.length++;
+        const node: Node<T> = { val: item, next: this.head };
+        this.head = node;
+    }
     pop(): T | undefined {
-
-}
+        if (!this.length) return;
+        this.length--;
+        const node = this.head;
+        this.head = node?.next;
+        return node?.val;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.val;
+    }
 }
