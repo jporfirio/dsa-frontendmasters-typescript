c42feb687ab90e326a44819edd17628ae96323fe
Jeann Porfírio <jeannporfirio@gmail.com>
2025-02-11 14:00:55 -0300

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
 
+    while (hi > lo) {
+        let mid = Math.floor(lo + (hi - lo) / 2);
+        if (haystack[mid] === needle) return true;
+
+        if (haystack[mid] > needle) {
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
+    }
+    return false;
 }
