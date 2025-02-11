b7b926292c33e6e325a779ab57eaf7a18108fea7
Jeann Porfírio <jeannporfirio@gmail.com>
2025-02-11 14:09:40 -0300

    bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,11 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let limit = arr.length; limit > 0; limit--) {
+        for (let index = 0; index < limit; index++) {
+            if (arr[index] > arr[index + 1]) {
+                let temp = arr[index];
+                arr[index] = arr[index + 1];
+                arr[index + 1] = temp;
+            }
+        }
+    }
 }
