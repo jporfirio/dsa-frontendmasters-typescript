0b9a0919744999212e6b147ab94f2f5ca75312b4
Jeann Porfírio <jeannporfirio@gmail.com>
2025-02-11 14:09:44 -0300

    queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,43 @@
+type Node<T> = {
+    val: T;
+    next?: Node<T> | null;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    public head: Node<T> | null;
+    public tail: Node<T> | null;
 
     constructor() {
+        this.length = 0;
+        this.head = null;
     }
 
     enqueue(item: T): void {
-
-}
+        const node: Node<T> = { val: item, next: null };
+        this.length++;
+        if (this.tail) {
+            this.tail.next = node;
+            this.tail = this.tail.next;
+        } else {
+            this.head = node;
+            this.tail = node;
+        }
+    }
     deque(): T | undefined {
-
-}
+        if (!this.head) return;
+        this.length--;
+        const node = this.head;
+        if (this.length) this.head = node.next ?? null;
+        else {
+            this.head = null;
+            this.tail = null;
+        }
+        node.next = null;
+        return node.val;
+    }
     peek(): T | undefined {
-
-}
+        if (this.head?.val) return this.head.val;
+        return;
+    }
 }
